d4277bed6171f5a43dfa9b2ac115073dd0e2db86
chandan <chandankumar.btele20@pec.edu.in>
2024-07-25 16:28:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//console is used to print something
+
+let a=5
+console.log(a)
+
+console.log("aryan"," naresh")
+console.log(9+8)
+console.log(6*9)
+
+
+//function
+let add=function(a,b){
+
+    console.log(a+b)
+}
+
+add(75,75)
+
+//function multiply
+let multiply=function(a,b){
+    console.log(a*b)
+}
+
+multiply(3,8)
+
+
